4f7ac2f692c4d26fd2ba494d3f00a98d28ec9a9e
MazenWael2004 <115092034+MazenWael2004@users.noreply.github.com>
2025-03-22 16:17:41 +0200

    Update Footer.php

--- Footer.php
+++ Footer.php
@@ -4,18 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-    
+    <title>Footer</title>
 </head>
 <body>
-    <div class="nav-bar">
-        <span class="first">
-            Verify
-        </span>
-         <span class="second">   
-             Me
-         </span>.
+    <div class="footer-container">
+        <div class="title">
+            Copyright &copy; 2025 VerifyMe,Inc
+        </div>
+
+        <div class="social-icons">
+            <img src="facebook.png" width="50px" height="50px" alt="">
+            <img src="instagram.png" width="50px" height="50px" alt="">
+            <img src="twitter.png"  width="50px" height="50px" alt="">
+            <img src="linkedin.png" alt="">
+        </div>
     </div>
-    
 </body>
 </html>
