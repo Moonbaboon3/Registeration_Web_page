44be772d6655db37ce00c5903857fc5fce6bb0eb
Omer Yassir <omerym10@gmail.com>
2025-03-24 17:19:00 +0200

    fix header links

--- Header.php
+++ Header.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
-    
+
 </head>
+
 <body>
     <div class="nav-bar">
         <div>
-        <span class="first">Verify</span><span class="second">Me</span>
+            <span class="first">Verify</span><span class="second">Me</span>
         </div>
         <div class="nav_items">
-        <a href="#home" class="active">Home</a>
-        <a href="#about">About</a>
-        <a href="#services">Services</a>
-        <a href="#portfolio">Portfolio</a>
-        <a href="#contact">Contact</a>
+            <a href="/#home" class="active">Home</a>
+            <a href="/#about">About</a>
+            <a href="/#services">Services</a>
+            <a href="/#portfolio">Portfolio</a>
+            <a href="/#contact">Contact</a>
         </div>
-       
+
     </div>
-    
+
 </body>
+
 </html>
