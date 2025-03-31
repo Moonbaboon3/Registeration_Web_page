f783322acaa06f999a3b04cdc093618f24e7e5e8
Moonbaboon3 <147054617+Moonbaboon3@users.noreply.github.com>
2025-03-31 23:08:07 +0200

    changed ajax server side implementation location
    the implementation is now in DB_Ops file . Deleted ajax.php .

--- DB_Ops.php
+++ DB_Ops.php
@@ -11,55 +11,71 @@ use Dom\Mysql;
     $conn = mysqli_connect($db_server, $db_user, $db_pass);
     mysqli_select_db($conn,$db_name);
     }
-
     catch(mysqli_sql_exception){
         echo "Could not connect!";
     }
-   
-    //$check = "moh21";
-    //gets input from user and adds it to query 
-    /*$stmt = mysqli_stmt_init($conn);
-    $query ="select COUNT(*) from user where user_name LIKE ?";
-    if(mysqli_stmt_prepare($stmt, $query))
-    {
-        mysqli_stmt_bind_param($stmt, "s", $check);
-        mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
-        if(mysqli_num_rows($result) > 0)
-        {
-            echo "username already exits";
-        }
-        else{
-            $sql = "INSERT INTO User (full_name, user_name, phone, whatsapp_number, address, password, email)
-            VALUES ('Mohammed Abdallah', 'moh21', '091' , '+249 966 327 998', 'Sheikh Zayied' , '2004' , 'mohamed@email.com')";
 
-            if($conn->query($sql) === TRUE){
-                echo "New record created successfully";
-            }else {
-                echo "Error:" . $sql . "<br>" . $conn->error;
+    //user starts typing in user_name input field
+    if(!empty($_POST{'user_name'})){
+        $username = $_POST['user_name'];
+        //retrieve from db matching row with same user_name
+        $query ="SELECT * FROM user WHERE user_name ='" .$_POST["user_name"] . "'";
+        $result = mysqli_query($conn, $query);
+        //return results as number of rows
+        $count = mysqli_num_rows($result);
+       
+
+            if($count> 0)
+            {
+                echo "<span style=' color: red; font-size: 0.9rem; font-weight: bold;' > Username is already taken</span>";
+                echo "<script>$('#submit').prop('disabled, true');</script>";
             }
+            else
+            {
+                echo "<span style='color: green; font-size: 0.9rem; font-weight: bold;'> Username is available to use</span>";
+                echo "<script>$('#submit').prop('disabled, false');</script>";
+            }
+           
         }
+    
+
+
+    
+   
+
+/*
+    if($_SERVER['REQUEST_METHOD'] === 'POST')
+    {
+    //gets input from user and adds it to query 
+    $full_name = $_POST['full_name'] ;
+    $user_name = $_POST['user_name'] ;
+    $phone     = $_POST['phone'] ;
+    $whatsnum  = $_POST['whatsapp_number'];
+    $address   = $_POST['address'];
+    $password  = $_POST['password'];
+    $email     = $_POST['email'];
+    $image     = $_POST['image'];
+
+    $sql = "INSERT INTO User 
+    VALUES ($full_name,$user_name, $phone, $whatsnum, $address , $password, $emai , $image)";
+
+    if($conn->query($sql) === TRUE)
+    {
+        echo "New record created successfully";
+    }
+    else 
+    {
+        echo "Error:" . $sql . "<br>" . $conn->error;
+    }
+        
 
         $conn->close();
-   }*/
-   
+   }
+*/  
     
     
     
     
-    /*$result = mysqli_query($conn,$query);
-    //if a result is found then the result is the table
-    if(mysqli_fetch_array($result)){
 
-        echo "This username already exists";
-    }
-    else{
-        //if not its creates a new query and adds it to the table
-        $query = 'insert into user_names values  ("'.$check.'")';
-        mysqli_query($conn,$query);
-        echo "thise username doesnt exist";
-    }
-    //close connection to database to free up space
-    mysqli_close($conn);*/
 
 ?>
--- check_username.js
+++ check_username.js
@@ -1,11 +1,10 @@
 function checkUsername(){
     jQuery.ajax({
-        url: "ajax.php",
+        url: "DB_Ops.php", 
         data:'user_name='+ $('#user_name').val(),
         type: 'POST',
         success:function(data){
             $('#check_username').html(data);
-           // $('#check-username').html(data);
         },
         error:function(){}
     });
